bdaf81b6208694600977dd99075900919f844554
Radamanthus Batnag <rad@batnag.org>
2019-05-12 22:31:47 +0800

    WIP: Add code to write to send records to Kinesis

--- log_drain.rb
+++ log_drain.rb
@@ -14,6 +14,24 @@ Cuba.define do
         if valid_logplex_headers?(logplex_headers)
           res.write "Message count: #{logplex_headers[:message_count]}"
           res.write "noot noot"
+
+          # TODO: Wrap this in a module
+          require 'aws-sdk-kinesis'
+          options = {
+            region: 'us-east-2'
+          }
+          client = Aws::Kinesis::Client.new(options)
+          records = [
+            {
+              data: "noot noot",
+              partition_key: "1"
+            }
+          ]
+          params = {
+            records: records,
+            stream_name: 'test'
+          }
+          client.put_records(params)
         end
       end
       res.write nil
